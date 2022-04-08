134ee1286bf202ce7c8b5315d656cec52a94ff9f
dfhj <dfhj>
2022-04-07 19:30:53 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
+ * main - Entry point
  *
- *Return: 0 okay
+ * Return: Always 0 (Success)
  */
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
+return (0);
